e38f1f99badf7786ef20fa3fa41d196a6071cdf1
Ramesh <raga2560@gmail.com>
2018-10-09 15:53:26 +0530

    prog

--- compositekeylibtest.js
+++ compositekeylibtest.js
@@ -2,6 +2,11 @@ bitcoin = require('bitcoinjs-lib');
 bitcoincontrol = require('bitcoincontrol');
 compositekeylib = bitcoincontrol.compositekeylib;
 
+/*
+
+This example uses bitcoin testnet
+
+*/
 
 var uidkeypair = bitcoin.ECPair.fromWIF(
  'cRgnQe1TQngWfnsLo9YUExBjx3iVKNHu2XogVF8JhHiKQTJVQHhk',
@@ -14,77 +19,96 @@ var tmpKeypair = bitcoin.ECPair.fromWIF(
 
 
 var onepubKey = tmpKeypair.getPublicKeyBuffer();
-var onepubKeyhex =  new Buffer(onepubKey, 'hex');
 
 var uidpubKey = uidkeypair.getPublicKeyBuffer();
-var uidpubKeyhex =  new Buffer(uidpubKey, 'hex');
+
+var addresstosend = '2MtmgQv5SgAUJHCVnVTT52EJrZCegVYTYiv';
+
+var toscriptPubKey =  bitcoin.address.toOutputScript(addresstosend, bitcoin.networks.testnet);
+
+console.log("The address to which all spent transactions is sent = \n"+ addresstosend);
+
+
+console.log("The uidpubKey used = "+ onepubKey.toString('hex'));
 		  
+console.log(".........First set of data ........");
+console.log(".........Buffer code address creation ........");
+console.log("The buffer code used = "+ onepubKey.toString('hex'));
+
+var addr1 = compositekeylib.getBufControlCodeAddress(onepubKey, uidpubKey,  bitcoin.networks.testnet);
+console.log("Buffer code address = "+addr1);
+
+console.log(".........Second set of data ........");
+console.log(".........String code address creation ........");
+var strcode = "hello";
+console.log("The string code used = "+ strcode);
+var addr2 = compositekeylib.getStrControlCodeAddress(strcode,  uidpubKey, bitcoin.networks.testnet);
+console.log("String code address = "+addr2);
 
 
-var addr1 = compositekeylib.getBufControlCodeAddress(tmpKeypair.getPublicKeyBuffer(), uidpubKey,  bitcoin.networks.testnet);
-console.log("address1 = "+addr1);
-
-var addr2 = compositekeylib.getStrControlCodeAddress('hello',  uidpubKey, bitcoin.networks.testnet);
-console.log("address2 = "+addr2);
-
-
+console.log(".........Third set of data ........");
+console.log(".........Custom contract address creation ........");
 var pubKeyHash = bitcoin.crypto.hash160(onepubKey) ; 
 
 var redeemScript =  bitcoin.script.compile([bitcoin.opcodes.OP_HASH160, pubKeyHash, 
 bitcoin.opcodes.OP_EQUAL ]) 
 
-console.log("redeemScript="+redeemScript.toString('hex'));
+console.log("Custom contract input = "+onepubKey.toString('hex'));
+console.log("Custom contract redeemScript = "+redeemScript.toString('hex'));
 
 var scriptPubKey = bitcoin.script.scriptHashOutput(bitcoin.crypto.hash160(redeemScript))
 
-console.log("scriptpubkey="+scriptPubKey.toString('hex'));
+console.log("scriptpubkey = "+scriptPubKey.toString('hex'));
 var address = bitcoin.address.fromOutputScript(scriptPubKey, bitcoin.networks.testnet)
 
 
 
 var addr3 = compositekeylib.getCustomContractAddress(redeemScript,  uidpubKey, bitcoin.networks.testnet);
-console.log("address3 = "+addr3);
+console.log("Custom contract address = "+addr3);
 
-// unlock using buffer code
+console.log(".......Spending from first address  ........");
+// Example to unlock using buffer code address
 var tx1 = "52a6d4903cd534b1902fdbe3073b5c983a2f59a5d48dff6211b39fdf5b1bac02";
 var code = tmpKeypair.getPublicKeyBuffer();
 var indextospend = 0;
 var sequence = 141155;
-var outscriptPubKey = scriptPubKey;
+var outscriptPubKey = toscriptPubKey;
 var amount = 141515;
 var tx = compositekeylib.getTransactionForunlockBufCode (code, uidpubKey,  tx1, indextospend, sequence, outscriptPubKey, amount);
 
-console.log("transaction to withdraw from addr1="+tx.toHex());
+console.log("transaction to withdraw from buffer code address =\n"+tx.toHex());
 
 
 
+console.log(".......Spending from second address  ........");
 
-// unlock using string code
+// Example to unlock using string code address
 var tx1 = "52a6d4903cd534b1902fdbe3073b5c983a2f59a5d48dff6211b39fdf5b1bac02";
 var code = "hello"
 var indextospend = 0;
 var sequence = 141155;
-var outscriptPubKey = scriptPubKey;
+var outscriptPubKey = toscriptPubKey;
 var amount = 141515;
 var tx = compositekeylib.getTransactionForunlockStrCode (code,  uidpubKey, tx1, indextospend, sequence, outscriptPubKey, amount);
 
-console.log("transaction to withdraw from addr2="+tx.toHex());
+console.log("transaction to withdraw from string code address =\n"+tx.toHex());
 
 
 
 
+console.log("..........Spending from third address  ..........");
 
-// unlock using custom contract input
+// Example to unlock using custom contract input address
 var tx1 = "52a6d4903cd534b1902fdbe3073b5c983a2f59a5d48dff6211b39fdf5b1bac02";
 var indextospend = 0;
 var sequence = 141155;
-var outscriptPubKey = scriptPubKey;
-var contractinput = tmpKeypair.getPublicKeyBuffer();
+var outscriptPubKey = toscriptPubKey;
+var contractinput = onepubKey.toString('hex');
 var amount = 141515;
 var tx = compositekeylib.getTransactionForCustomContract(contractinput, uidpubKey,redeemScript,
   tx1, indextospend, sequence, outscriptPubKey, amount);
 
-console.log("transaction to withdraw from addr3="+tx.toHex());
+console.log("transaction to withdraw from custom contract address =\n"+tx.toHex());
 
 
 
