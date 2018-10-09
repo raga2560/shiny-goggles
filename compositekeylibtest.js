203ab605bd5904974b9a95dc9d2a464852849b2a
Ramesh <raga2560@gmail.com>
2018-10-09 09:15:23 +0530

    added missing file

--- compositekeylibtest.js
+++ compositekeylibtest.js
@@ -1,44 +1,34 @@
 bitcoin = require('bitcoinjs-lib');
 bitcoincontrol = require('bitcoincontrol');
 compositekeylib = bitcoincontrol.compositekeylib;
-bufferReverse = require('buffer-reverse')
 
-console.log("test");
-// deterministic RNG for testing only
-function rng () { return Buffer.from('zzzttyyzzzzzzzzzzzzzzzzuiozzzzzz') }
-
-
-
-
-  var keyPair = bitcoin.ECPair.makeRandom({ network: bitcoin.networks.testnet, rng: rng })
-
-console.log(keyPair.toWIF());
 
 var uidkeypair = bitcoin.ECPair.fromWIF(
  'cRgnQe1TQngWfnsLo9YUExBjx3iVKNHu2XogVF8JhHiKQTJVQHhk',
  bitcoin.networks.testnet);
 
 
-var keyPair1 = bitcoin.ECPair.fromWIF(
+var tmpKeypair = bitcoin.ECPair.fromWIF(
 'cRgnQe1TQngWfnsLo9YUExBjx3iVKNHu2ZfiRcUivATuojDdzdus',
  bitcoin.networks.testnet);
-var keyPair2 = bitcoin.ECPair.fromWIF(
-      '91avARGdfge8E4tZfYLoxeJ5sGBdNJQH4kvjJoQFacbgx3cTMqe',
-       bitcoin.networks.testnet);
-var keyPair3 = bitcoin.ECPair.fromWIF(
-      '91avARGdfge8E4tZfYLoxeJ5sGBdNJQH4kvjJoQFacbgww7vXtT',
- bitcoin.networks.testnet);
 
-var onepubKey = keyPair1.getPublicKeyBuffer();
+
+var onepubKey = tmpKeypair.getPublicKeyBuffer();
 var onepubKeyhex =  new Buffer(onepubKey, 'hex');
 
 var uidpubKey = uidkeypair.getPublicKeyBuffer();
 var uidpubKeyhex =  new Buffer(uidpubKey, 'hex');
 		  
 
-var pubKeyHash = bitcoin.crypto.hash160(keyPair1.getPublicKeyBuffer());
+
+var addr1 = compositekeylib.getBufControlCodeAddress(tmpKeypair.getPublicKeyBuffer(), uidpubKey,  bitcoin.networks.testnet);
+console.log("address1 = "+addr1);
+
+var addr2 = compositekeylib.getStrControlCodeAddress('hello',  uidpubKey, bitcoin.networks.testnet);
+console.log("address2 = "+addr2);
 
 
+var pubKeyHash = bitcoin.crypto.hash160(onepubKey) ; 
 
 var redeemScript =  bitcoin.script.compile([bitcoin.opcodes.OP_HASH160, pubKeyHash, 
 bitcoin.opcodes.OP_EQUAL ]) 
@@ -47,28 +37,24 @@ console.log("redeemScript="+redeemScript.toString('hex'));
 
 var scriptPubKey = bitcoin.script.scriptHashOutput(bitcoin.crypto.hash160(redeemScript))
 
-console.log("sscriptpubkey="+scriptPubKey.toString('hex'));
+console.log("scriptpubkey="+scriptPubKey.toString('hex'));
 var address = bitcoin.address.fromOutputScript(scriptPubKey, bitcoin.networks.testnet)
 
 
-var addr1 = compositekeylib.getBufControlCodeAddress(keyPair1.getPublicKeyBuffer(), uidpubKey,  bitcoin.networks.testnet);
-console.log("address1 = "+addr1);
-var addr2 = compositekeylib.getStrControlCodeAddress('hello',  uidpubKey, bitcoin.networks.testnet);
-console.log("address2 = "+addr2);
 
 var addr3 = compositekeylib.getCustomContractAddress(redeemScript,  uidpubKey, bitcoin.networks.testnet);
 console.log("address3 = "+addr3);
 
 // unlock using buffer code
 var tx1 = "52a6d4903cd534b1902fdbe3073b5c983a2f59a5d48dff6211b39fdf5b1bac02";
-var code = keyPair1.getPublicKeyBuffer();
+var code = tmpKeypair.getPublicKeyBuffer();
 var indextospend = 0;
 var sequence = 141155;
 var outscriptPubKey = scriptPubKey;
 var amount = 141515;
 var tx = compositekeylib.getTransactionForunlockBufCode (code, uidpubKey,  tx1, indextospend, sequence, outscriptPubKey, amount);
 
-console.log(tx.toHex());
+console.log("transaction to withdraw from addr1="+tx.toHex());
 
 
 
@@ -82,7 +68,7 @@ var outscriptPubKey = scriptPubKey;
 var amount = 141515;
 var tx = compositekeylib.getTransactionForunlockStrCode (code,  uidpubKey, tx1, indextospend, sequence, outscriptPubKey, amount);
 
-console.log(tx.toHex());
+console.log("transaction to withdraw from addr2="+tx.toHex());
 
 
 
@@ -93,12 +79,12 @@ var tx1 = "52a6d4903cd534b1902fdbe3073b5c983a2f59a5d48dff6211b39fdf5b1bac02";
 var indextospend = 0;
 var sequence = 141155;
 var outscriptPubKey = scriptPubKey;
-var contractinput = keyPair1.getPublicKeyBuffer();
+var contractinput = tmpKeypair.getPublicKeyBuffer();
 var amount = 141515;
 var tx = compositekeylib.getTransactionForCustomContract(contractinput, uidpubKey,redeemScript,
   tx1, indextospend, sequence, outscriptPubKey, amount);
 
-console.log(tx.toHex());
+console.log("transaction to withdraw from addr3="+tx.toHex());
 
 
 
